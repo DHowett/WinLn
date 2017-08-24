db3e33ff15cfea7a7be8ed33f7a7643e035e18ad
Dustin L. Howett <dustin@howett.net>
2017-08-23 22:20:06 -0700

    Add usage, replace --symlink -> --symbolic, add --relative

--- WinLn/WinLn.cpp
+++ WinLn/WinLn.cpp
@@ -3,7 +3,16 @@
 #include <stdio.h>
 
 static void usage(wchar_t* pname) {
-	fwprintf(stderr, L"Usage: %ls [option] TARGET LINK_NAME\r\n", pname);
+	fwprintf(stderr, L"Usage: %ls [option]... <target> <link>\r\n"
+		             L"\r\n"
+		             L"  -s, --symbolic     create symbolic links instead of hard links\r\n"
+		             L"  -j, --junction     create a Windows directory junctions instead of hard links\r\n"
+		             L"                     -s and -j are mutually exclusive\r\n"
+		             L"\r\n"
+		             L"  -r, --relative     create symbolic links relative to link location\r\n"
+		             L"  -f, --force        remove existing destination files\r\n"
+		             L"  -h, --help         display this help\r\n"
+		, pname);
 }
 
 template <typename... Args>
@@ -90,15 +99,16 @@ static int getopt_long(int argc, wchar_t** argv, const option opts[], int* idxpt
 
 static option opts[]{
 	{L"force", L'f', false},
-	{L"symlink", L's', false},
+	{L"symbolic", L's', false},
 	{L"junction", L'j', false},
+	{L"relative", L'r', false},
 	{L"help", L'h', false},
 	//{L"no-target-directory", L'T', false},
 	{nullptr, 0, false},
 };
 
 int wmain(int argc, wchar_t** argv) {
-	bool force = false, symlink = false, junction = false;
+	bool force = false, symlink = false, junction = false, relative = false;
 	int optind = 0;
 	while(int o = getopt_long(argc, argv, opts, &optind, nullptr)) {
 		switch(o) {
@@ -107,22 +117,31 @@ int wmain(int argc, wchar_t** argv) {
 		case 'f':
 			force = true;
 			break;
-		case 's':
-			symlink = true;
-			break;
-		case 'j':
-			junction = true;
-			break;
 		case 'h':
 			usage(argv[0]);
 			return 0;
+		case 'j':
+			junction = true;
+			break;
+		case 'r':
+			relative = true;
+			break;
+		case 's':
+			symlink = true;
+			break;
 		}
 	}
 opts_done:
 
 	if(symlink && junction) {
-		argerr(argv[0], L"cannot create symbolic links and junctions at the same time");
+		argerr(argv[0], L"cannot do --symbolic and --junction at the same time");
 		return 1;
+	}
+
+	if(relative && !symlink) {
+		argerr(argv[0], L"cannot do --relative without --symbolic");
+		return 1;
+
 	}
 
 	if(optind + 2 > argc) {
@@ -149,6 +168,12 @@ opts_done:
 	}
 
 	if(symlink) {
+		// TODO(DH): Junctions
+		if(relative) {
+			argerr(argv[0], L"--relative isn't yet implemented");
+			return 1;
+		}
+
 		int flags = 0;
 		if((targetFi.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0) {
 			flags |= SYMBOLIC_LINK_FLAG_DIRECTORY;
@@ -158,7 +183,9 @@ opts_done:
 			return exit_last_error(L"creating the symlink");
 		}
 	} else if(junction) {
-		// 
+		// TODO(DH): Junctions
+		argerr(argv[0], L"junctions aren't supported");
+		return 1;
 	} else {
 		if(!CreateHardLinkW(linkname, target, nullptr)) {
 			return exit_last_error(L"creating the hard link");
