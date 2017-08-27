0edb4174f73d443b6562c8969286b9bddc2f2d75
Dustin L. Howett <dustin@howett.net>
2017-08-27 15:25:45 -0700

    [getopt_tests] Flesh out the option tests

--- getopt_tests/getopt_tests.cpp
+++ getopt_tests/getopt_tests.cpp
@@ -11,56 +11,103 @@ static option opts[] = {
 	{L"longname_c", L'c', false},
 };
 
+struct testcase {
+	std::vector<wchar_t*> cmdline;
+	std::vector<int> expectedResponses;
+	std::vector<std::wstring> expectedOptionArguments;
+	std::vector<std::wstring> expectedRemainingArguments;
+};
 
+class GetoptTest: public ::testing::TestWithParam<struct testcase> {
+public:
+	void SetUp() {
+		auto& testcase = GetParam();
+		std::vector<wchar_t*> copiedCmdline{testcase.cmdline};
+		std::vector<int> responses;
+		std::vector<std::wstring> arguments;
+		int o = 0;
+		_optreset = true;
+		while((o = getopt_long(copiedCmdline.size(), copiedCmdline.data(), opts)) != -1) {
+			responses.emplace_back(o);
+			if(optarg) arguments.emplace_back(optarg);
+		}
 
-TEST(Getopt, ShortOptions) {
-	wchar_t* argv[] = {
-		L"ProgramName",
-		L"Argument 1",
-		L"-s", // short option
-		L"Argument 2",
-		L"-a", // short option that has following parameter
-		L"param1",
-		L"Argument 3",
-		L"-aparam2", // short option that has inline parameter
-		L"-bc", // Pack
-		L"-bcaparam3", // pack with inline parameter
-		L"-bca", // pack with following parameter
-		L"param4",
-		L"Argument 4",
-		L"--", // end of options
-		L"-bcaparam5",
-		L"Argument 5",
-	};
+		const std::vector<std::wstring> afterOptind(copiedCmdline.begin() + optind, copiedCmdline.end());
 
-	std::vector<int> responses;
-	std::vector<std::wstring> arguments;
-
-	int o = 0;
-	_optreset = true;
-	while((o = getopt_long(std::extent<decltype(argv)>::value, argv, opts)) != -1) {
-		responses.emplace_back(o);
-		if(optarg) arguments.emplace_back(optarg);
+		_responses = std::move(responses);
+		_arguments = std::move(arguments);
+		_remainingParameters = std::move(afterOptind);
 	}
-	const std::vector<std::wstring> afterOptind(argv + optind, argv + std::extent<decltype(argv)>::value);
+protected:
+	std::vector<int> _responses;
+	std::vector<std::wstring> _arguments;
+	std::vector<std::wstring> _remainingParameters;
+};
 
-	std::vector<int> expectedResponses{'s', 'a', 'a', 'b', 'c', 'b', 'c', 'a', 'b', 'c', 'a',};
-	std::vector<std::wstring> expectedArguments{
-		L"param1",
-		L"param2",
-		L"param3",
-		L"param4",
-	};
-	std::vector<std::wstring> expectedAfterOptind{
-		L"Argument 1",
-		L"Argument 2",
-		L"Argument 3",
-		L"Argument 4",
-		L"-bcaparam5",
-		L"Argument 5",
-	};
-
-	ASSERT_EQ(expectedResponses, responses);
-	EXPECT_EQ(expectedArguments, arguments);
-	EXPECT_EQ(expectedAfterOptind, afterOptind);
+TEST_P(GetoptTest, ReturnsOptions) {
+	auto& testcase = GetParam();
+	ASSERT_EQ(testcase.expectedResponses, _responses);
 }
+
+TEST_P(GetoptTest, ReturnsOptionArguments) {
+	auto& testcase = GetParam();
+	EXPECT_EQ(testcase.expectedOptionArguments, _arguments);
+}
+
+TEST_P(GetoptTest, SortsAndReturnsNonOptionParameters) {
+	auto& testcase = GetParam();
+	EXPECT_EQ(testcase.expectedRemainingArguments, _remainingParameters);
+}
+
+struct testcase cases[]{
+	{ // No options
+		/* argv */ {L"ProgramName", L"Argument 1"},
+		/* resp */ {},
+		/* args */ {},
+		/* left */ {L"Argument 1"},
+	},
+	{ // Short Options
+		/* argv */ {L"ProgramName", L"Argument 1", L"-s", L"-b", L"-c", L"-a", L"hello", L"Argument 2"},
+		/* resp */ {L's', 'b', 'c', 'a'},
+		/* args */ {L"hello"},
+		/* left */ {L"Argument 1", L"Argument 2"},
+	},
+	{ // Short Options - Packed
+		/* argv */ {L"ProgramName", L"Argument 1", L"-sbc", L"-ahello", L"Argument 2"},
+		/* resp */ {L's', 'b', 'c', 'a'},
+		/* args */ {L"hello"},
+		/* left */ {L"Argument 1", L"Argument 2"},
+	},
+	{ // Short Options - Packed and Unpacked
+		/* argv */ {L"ProgramName", L"Argument 1", L"-s", L"-bc", L"-sbahello", L"Argument 2", L"-s"},
+		/* resp */ {L's', 'b', 'c', 's', 'b', 'a', 's'},
+		/* args */ {L"hello"},
+		/* left */ {L"Argument 1", L"Argument 2"},
+	},
+	{ // Long Options
+		/* argv */ {L"ProgramName", L"Argument 1", L"--short", L"--longname_a=value", L"--longname_a", L"value 2", L"Argument 2", L"--longname_b"},
+		/* resp */ {L's', 'a', 'a', 'b'},
+		/* args */ {L"value", L"value 2"},
+		/* left */ {L"Argument 1", L"Argument 2"},
+	},
+	{ // Long Options, Interspersed with Short Options
+		/* argv */ {L"ProgramName", L"Argument 1", L"--short", L"-avalue", L"--longname_a", L"value 2", L"Argument 2", L"-bc"},
+		/* resp */ {L's', 'a', 'a', 'b', 'c'},
+		/* args */ {L"value", L"value 2"},
+		/* left */ {L"Argument 1", L"Argument 2"},
+	},
+	{ // -- (before all options)
+		/* argv */ {L"ProgramName", L"--", L"Argument 1", L"--short", L"-avalue", L"--longname_a", L"value 2", L"Argument 2", L"-bc"},
+		/* resp */ {},
+		/* args */ {},
+		/* left */ {L"Argument 1", L"--short", L"-avalue", L"--longname_a", L"value 2", L"Argument 2", L"-bc"},
+	},
+	{ // -- (after some options)
+		/* argv */ {L"ProgramName",  L"Argument 1", L"--short", L"-avalue", L"--longname_a", L"value 2", L"--", L"Argument 2", L"-bc"},
+		/* resp */ {'s', 'a', 'a'},
+		/* args */ {L"value", L"value 2"},
+		/* left */ {L"Argument 1", L"Argument 2", L"-bc"},
+	},
+};
+
+INSTANTIATE_TEST_CASE_P(Getopt, GetoptTest, ::testing::ValuesIn(cases));
